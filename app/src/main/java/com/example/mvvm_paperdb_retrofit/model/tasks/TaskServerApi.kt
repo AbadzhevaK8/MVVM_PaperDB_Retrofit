4cd561a8ed789cf09c8e3b46d418f5c035335bee
k8abadzheva <make.me.loco@gmail.com>
2025-03-10 23:14:27 +0300

    feat: add getTaskByID fun to TaskServerApi.

--- app/src/main/java/com/example/mvvm_paperdb_retrofit/model/tasks/TaskServerApi.kt
+++ app/src/main/java/com/example/mvvm_paperdb_retrofit/model/tasks/TaskServerApi.kt
@@ -3,11 +3,26 @@ package com.example.mvvm_paperdb_retrofit.model.tasks
 import com.example.mvvm_paperdb_retrofit.model.MyCustomCallBack
 import com.example.mvvm_paperdb_retrofit.retrofit.RetrofitService
 import com.example.mvvm_paperdb_retrofit.retrofit.TaskServerInterface
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class TaskServerApi : TaskInterface {
     private val service = RetrofitService.retrofit.create(TaskServerInterface::class.java)
     override fun getTaskById(id: String, callBack: MyCustomCallBack<TaskModel>) {
-        TODO("Not yet implemented")
+        try {
+            service.getTaskById(id).enqueue(object : Callback<TaskModel> {
+                override fun onResponse(p0: Call<TaskModel>, p1: Response<TaskModel>) {
+                    callBack.onSuccess(p1.body() ?: TaskModel())
+                }
+
+                override fun onFailure(p0: Call<TaskModel>, p1: Throwable) {
+                    callBack.onFailure(p1.toString())
+                }
+            })
+        } catch (ex: Exception) {
+            callBack.onFailure(ex.toString())
+        }
     }
 
     override fun getTasks(callBack: MyCustomCallBack<TaskModel>) {
